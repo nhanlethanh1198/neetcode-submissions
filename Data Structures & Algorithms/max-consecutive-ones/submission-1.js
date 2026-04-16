0194a9f9f87bc4dfcc7c217f4923c177e6b95b9d
Le Thanh Nhan <50665977+nhanlethanh1198@users.noreply.github.com>
2026-04-16 20:09:53 +0700

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let res = 0
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                res = 0
+            } else {
+                res = Math.max(res, 1)
+            }
+        }
+
+        return res
+
+    }
+}
