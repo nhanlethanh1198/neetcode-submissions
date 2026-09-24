f7045b6458bacde49d790397a8c37b06e0540d5d
Le Thanh Nhan <50665977+nhanlethanh1198@users.noreply.github.com>
2026-09-24 15:47:38 +0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false
+
+        const a = Array.from(a).sort(), b = Array.from(b).sort()
+
+        return a.toString() == b.toString()
+    }
+}
